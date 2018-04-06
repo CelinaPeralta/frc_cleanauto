90fdbe797b215942dacf58a268e3d4df6a5f16d2
CelinaPeralta <celper19@bergen.org>
2018-04-06 08:00:50 -0400

    pdp

--- src/org/usfirst/frc/team6203/robot/commands/TestAuto.java
+++ src/org/usfirst/frc/team6203/robot/commands/TestAuto.java
@@ -11,15 +11,8 @@ public class TestAuto extends CommandGroup {
 	int switch_position = 0;
 
 	public TestAuto() {
-//		addSequential(new DriveAndRaiseElevator(0.8, 3.0, 0.5, 2.5));
-//		addSequential(new Wait(delay));
 		addSequential(new TurnToSetpoint(-90));
 		addSequential(new Wait(2));
 		addSequential(new TurnToSetpoint(90));
-//		addSequential(new Wait(delay));
-//		addSequential(new DriveToTimeout(0.4, 0.4, 1.0));
-//		addSequential(new Wait(delay));
-//		addSequential(new SetIntake(-0.4, 0.75));
-//		addSequential(new DriveToTimeout(-0.25, -0.25, 1.0));
 	}
 }
